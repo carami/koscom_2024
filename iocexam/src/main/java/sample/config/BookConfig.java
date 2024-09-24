afa3bbd132abb04581b6cfb4b36b60b35c9d9a08
carami <carami@nate.com>
2024-09-24 12:39:39 +0900

    sample 추가

---
+++ iocexam/src/main/java/sample/config/BookConfig.java
@@ -0,0 +1,11 @@
+package sample.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "sample.bean")
+public class BookConfig {
+//    @Bean
+//    public Book book(){
+//        return new Book();
+//    }
+}
