b4702e466adcc5b8b3555c640e588e7c3944d65d
carami <carami@nate.com>
2024-09-25 13:54:34 +0900

    update exam

---
+++ jpaexam/src/main/java/com/example/jpaexam/JpaexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaexamApplication.class, args);
+    }
+
+}
